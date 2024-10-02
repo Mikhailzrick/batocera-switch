1b565e289b16ee50293388161811ceba9a6ff392
Mikhailzrick <michael.shryock@gmail.com>
2024-10-02 07:26:05 -0500

    Update batocera-switch-updater.sh

--- system/switch/extra/batocera-switch-updater.sh
+++ system/switch/extra/batocera-switch-updater.sh
@@ -1585,6 +1585,7 @@ if [ -f "$link_ryujinxavalonia" ]; then
 	else
 		echo -e "${T}██ ${C}   ${F}RYUJINX-AVALONIA   [${W}!!${T}]   place test-ava-ryujinx-1.1.1403-linux_x64.tar.gz in /userdata/system/switch/"	
 	fi
+fi	
 #
 #
 # ---------------------------------------------------------------------------------- 
